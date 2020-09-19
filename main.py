6b8237a69cae2695efbdcc5ad2aa8901a2faf8d5
Isaac Lew <lewyzisaac@gmail.com>
2020-09-19 14:37:41 +0000

    successfully write result into ouput file

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-def countandwrite(file):
-    with open(file) as text : 
+def countandwrite(f1, f2):
+    with open(f1) as text : 
         aline = text.readlines()
         worddict = dict()
         for data in aline :
             words = data.split()
+            for word in words : 
+                word = word.lower()
+                if word in worddict :
+                    worddict[word] += 1
+                else : 
+                    worddict[word] = 1
+        outfile = open(f2, 'w')
+        wdlst = list(worddict.items()).sort()
+        for kv in list(worddict.items()) : 
+            for dt in kv : 
+                outfile.write(str(dt)+' ')
+            outfile.write('\n')
+        outfile.close()
+
+    return(worddict)
+                #print(word)
             #print(data)
             #print(words)
-
-countandwrite('alice.txt')
+countandwrite('alice.txt','out.txt')
+#print(countandwrite('alice.txt','out.txt'))
